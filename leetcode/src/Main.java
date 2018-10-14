fe8d528332fdef262383312fb241d4333c8cabdd
peng <527453497@qq.com>
2018-10-14 12:23:42 +0800

    栈:题目20

--- leetcode/src/Main.java
+++ leetcode/src/Main.java
@@ -1,6 +1,9 @@
+import validparentheses.Solution;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Solution solution = new Solution();
+        System.out.println(solution.isValid("()"));
     }
 }
