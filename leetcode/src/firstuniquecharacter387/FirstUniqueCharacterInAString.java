4af7259da92fb999ccdc0340567a1f30265c0446
peng <527453497@qq.com>
2019-01-02 15:44:17 +0800

    Update FirstUniqueCharacterInAString.java

--- leetcode/src/firstuniquecharacter387/FirstUniqueCharacterInAString.java
+++ leetcode/src/firstuniquecharacter387/FirstUniqueCharacterInAString.java
@@ -1,5 +1,18 @@
 package firstuniquecharacter387;
 
+/*给定一个字符串，找到它的第一个不重复的字符，并返回它的索引。如果不存在，则返回 -1。
+
+案例:
+
+s = "leetcode"
+返回 0.
+
+s = "loveleetcode",
+返回 2.
+ 
+
+注意事项：您可以假定该字符串只包含小写字母。*/
+
 public class FirstUniqueCharacterInAString {
 	
 	public int firstUniqChar(String s) {
